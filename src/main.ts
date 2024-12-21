18dddd6275559f1a58f75f094d2eaef06b9c752c
Wendy <1155107790@link.cuhk.edu.hk>
2024-12-21 20:39:11 +0000

    :sparkles: feat: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({ forbidUnknownValues: true, whitelist: true }),
     );
+    // swagger
+    const options = new DocumentBuilder()
+        .setTitle('API Documentation')
+        .setDescription('API documentation for the NestJS application')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-doc', app, document);
 
     await app.listen(process.env.PORT);
 }
